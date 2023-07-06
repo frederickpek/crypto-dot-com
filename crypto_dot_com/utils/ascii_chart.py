037d8da29d7a0acd20d0e350a4b7eb5237807417
Frederick Pek <ssixthlhm@gmail.com>
2023-07-06 22:44:56 +0800

    add ascii chart

---
+++ crypto_dot_com/utils/ascii_chart.py
@@ -0,0 +1,51 @@
+from typing import List
+from crypto_dot_com.asciichartpy import plot
+
+MAX_HEIGHT = 12
+
+
+def gen_ascii_plot(points: List[float]) -> str:
+    output = plot(series=points, cfg={
+        'height': MAX_HEIGHT,
+        'offset': 1,
+    })
+    x_axis = ''
+    interval = 6
+    n = len(points)
+    for i in range(1, n // interval + 1):
+        x_axis += '{: >{}}'.format(str(i * interval)[::-1], interval)
+    output += '\n' + '{: >{}}'.format(x_axis[::-1], n)
+    output += f'\nMin: ${min(points):,.2f}'
+    output += f'\nMax: ${max(points):,.2f}'
+    return output
+
+def _gen_ascii_plot(points: List[float]) -> str:
+    n = len(points)
+    if n <= 1:
+        return ''
+
+    grid = [[' '] * n for _ in range(MAX_HEIGHT)]
+
+    max_value = max(points)
+    min_value = min(points)
+    interval = (max_value - min_value) / (MAX_HEIGHT - 1)
+
+    def get_j(point):
+        btm_up_i = -1
+        curr_val = min_value - interval / 2
+        while (point > curr_val):
+            btm_up_i += 1
+            curr_val += interval
+        return MAX_HEIGHT - 1 - btm_up_i
+
+    for i, point in enumerate(points):
+        j = get_j(point)
+        grid[j][i] = '*'
+
+    output = f'^ ${max_value:,.2f}'
+    for row in grid:
+        output += '\n|' + ''.join(row)
+    output += f'\n| ${min_value:,.2f}'
+    output += '\n' + '-' * n + '>'
+
+    return output
--- lambda_function.py
+++ lambda_function.py
@@ -1,21 +1,30 @@
 import json
 import time
+import asyncio
 import numpy as np
 import pandas as pd
 from datetime import datetime
 from zoneinfo import ZoneInfo
 
-from crypto_dot_com.sdk.api import CdcApi
+from crypto_dot_com.sdk.async_api import CdcAsyncApi
 from crypto_dot_com.secret import API_KEY, SECRET_KEY
+from crypto_dot_com.utils.ascii_chart import gen_ascii_plot
 from crypto_dot_com.utils.telegram_bot import telegram_bot_sendtext
 
 
 def lambda_handler(event, context):
     start_time = time.time()
-    api = CdcApi(api_key=API_KEY, secret_key=SECRET_KEY)
+    api = CdcAsyncApi(api_key=API_KEY, secret_key=SECRET_KEY)
 
-    user_balance = api.get_user_balance()
-    tickers = api.get_tickers()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    user_balance_history, user_balance, tickers = loop.run_until_complete(
+        asyncio.gather(
+            api.get_user_balance_history(timeframe='H1', limit=100),
+            api.get_user_balance(),
+            api.get_tickers()
+        )
+    )
 
     tickers_map = {ticker['i']: ticker for ticker in tickers}
     total_cash_balance = float(user_balance['total_cash_balance'])
@@ -23,20 +32,26 @@ def lambda_handler(event, context):
 
     m = {'5m': 5, '1h': 60, '6h': 60 * 6, '24h': 60 * 24}
 
-    for position_balance in position_balances:
+    async def update_price_changes(position_balance: dict):
         ccy = position_balance['ccy']
         ticker = tickers_map.get(f'{ccy}_USDT') or tickers_map.get(f'{ccy}_USD')
         if ticker:
             instrument_name = ticker['i']
             bid, ask = float(ticker['b']), float(ticker['k'])
             price = (bid + ask) / 2
-            candlesticks = api.get_candlestick(instrument_name, timeframe='5m', count=300)
+            candlesticks = await api.get_candlestick(instrument_name, timeframe='5m', count=300)
             candlesticks.sort(reverse=True, key=lambda d: d['t'])
             base_interval = 5
             for key, interval in m.items():
-                open_price = float(candlesticks[interval // base_interval - 1]['o'])
+                open_price = float(candlesticks[interval // base_interval]['o'])
                 percentage = (price - open_price) / price * 100
                 position_balance[key] = f'{percentage:,.1f}'
+
+    loop.run_until_complete(
+        asyncio.gather(
+            *[update_price_changes(position_balance) for position_balance in position_balances]
+        )
+    )
 
     df = pd.DataFrame(position_balances)
     df['market_value'] = df['market_value'].map(lambda v: float(v))
@@ -45,17 +60,22 @@ def lambda_handler(event, context):
     df = df[['ccy', 'notional', *m.keys()]]
     df = df.replace(np.nan, '')
 
+    user_balance_history.sort(key=lambda d: d['t'])
+    points = list(map(lambda d: float(d['c']), user_balance_history))
+    chart = gen_ascii_plot(points=points[-33:])
+    curr_balance = f'Exch Balance: ${total_cash_balance:,.2f}'
+
     time_fmt = "%d %B %Y, %H:%M %p"
     time_zone = ZoneInfo('Asia/Singapore')
     dt = datetime.now(tz=time_zone).strftime(time_fmt)
     table = df.to_string(index=False).replace('_', '-')
+    
     end_time = time.time()
     duration = f'[finished in {end_time - start_time:,.3f}s]'
-    msg = f'``` {dt}\n\n{table}\n\n${total_cash_balance:,.2f}\n\n{duration}```'
+    msg = f'``` {dt}\n\n{table}\n\n{curr_balance}\n\n{chart}\n\n{duration}```'
     resp = telegram_bot_sendtext(msg)
 
     return {
         'statusCode': 200,
         'body': json.dumps(resp)
     }
-
