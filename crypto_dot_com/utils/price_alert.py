2e913ab6b0858808ff9a84ae95086b5856173d4e
Frederick Pek <ssixthlhm@gmail.com>
2024-01-17 23:21:43 +0800

    add price to alerts

--- crypto_dot_com/utils/price_alert.py
+++ crypto_dot_com/utils/price_alert.py
@@ -5,38 +5,41 @@ from zoneinfo import ZoneInfo
 from crypto_dot_com.utils.telegram_bot import telegram_bot_sendtext
 from crypto_dot_com.secret import PRICE_CHANGE_ALERT_BOT_TOKEN
 
-TRIGGERS = {
-    ("5m", 0.7, 1),
-    ("15m", 1.0, 3),
-    ("1h", 3.0, 12),
-}
+TRIGGERS = [
+    (" 5m ", 0.75, 1),
+    (" 15m ", 1.15, 3),
+    (" 1h ", 3.2, 12),
+]
 
 
 def alert_price_changes(ccy_candlesticks: dict):
     alerts = list()
     for ccy, candlesticks in ccy_candlesticks.items():
         candlesticks.sort(reverse=True, key=lambda d: d["t"])
+        end = candlesticks[1]
+        close_price = float(end["c"])
+        ccy_info = {
+            "ccy": ccy,
+            "price": f"${close_price:,.2f}",
+        }
+        triggered = False
         for interval, percentage_trigger, period in TRIGGERS:
-            end = candlesticks[1]
             start = candlesticks[period]
             open_price = float(start["o"])
-            close_price = float(end["c"])
             price_change = close_price - open_price
             percentage_price_change = price_change / open_price * 100
-            if abs(percentage_price_change) < percentage_trigger:
-                continue
-            alerts.append(
-                {
-                    "ccy": ccy,
-                    "interval": interval,
-                    "price_change": f"{percentage_price_change:,.2f}%",
-                }
-            )
+            ccy_info[interval] = f" {percentage_price_change:,.2f} "
+            if abs(percentage_price_change) > percentage_trigger:
+                ccy_info[interval] = f"({ccy_info[interval].strip()})"
+                triggered = True
+        if triggered:
+            alerts.append(ccy_info)
+
     if not alerts:
         return
 
     alert_df = pd.DataFrame(alerts)
-    alert_df = alert_df.sort_values(by="ccy")
+    alert_df = alert_df.sort_values(by=["price"])
     alert_table = alert_df.to_string(index=False)
 
     time_fmt = " %d %B %Y, %H:%M %p"
